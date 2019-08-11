a61bb9af6e34a6454173239a4c672413cd6a4466
Adeshkyal31 <adesh.kyal@example.com>
2019-08-12 00:16:08 +0530

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
  axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'];
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 import axios from "axios";
 
 export function setTokenHeader(token) {
