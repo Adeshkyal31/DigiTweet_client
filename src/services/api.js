7b523fd9ff69edbc6b33acadd6028e3c68cfde11
Adeshkyal31 <adesh.kyal@example.com>
2019-08-12 00:51:13 +0530

    update api 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post["Content-Type"] ="application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 export function setTokenHeader(token) {
   if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
